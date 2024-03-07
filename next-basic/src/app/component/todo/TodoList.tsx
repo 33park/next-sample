673bd24dcf20902599fd76d24613b4275358792e
sohyunpark <33.sohyun.park@gmail.com>
2024-03-07 17:47:17 +0900

    input checkBox 클릭 체크 여부

--- next-basic/src/app/component/todo/TodoList.tsx
+++ next-basic/src/app/component/todo/TodoList.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+"use client"
+
+import React,{useState} from 'react';
 import styled from 'styled-components';
 import { flexBox } from '@/style/styles/common';
 import { theme } from '@/style/styles/theme';
@@ -12,20 +14,27 @@ interface TodoProps {
 }
 
 export default function TodoList({ registeredList }: TodoProps) {
+
+
     const getOrderColor = (order: number) => {
         const colors = ['#FFE5A8', '#F8FF97', '#BCFFA4', '#A6FFD3', '#B9E3FF'];
         return colors[order % 5];
     };
+    const toggleCheckBox = (index: number) => {
+        const newList = [...registeredList];
+        newList[index].status = !newList[index].status;
+        console.log('checked', newList[index].status);
+    }
 
     return (
         <ListContainer>
-            {registeredList.map(({ order, content, status }) => (
+            {registeredList.map(({ order, content, status },index) => (
                 <TaskItem key={order}>
-                    <Order color={getOrderColor(order)} isChecked={status}>{order}</Order>
-                    <Content isChecked={status}>{content}</Content>
+                    <Order color={getOrderColor(order)} $isChecked={status}>{order}</Order>
+                    <Content $isChecked={status}>{content}</Content>
                     <MoreHorizontal stroke={theme.colors.gray} />
-                    <CheckboxContainer isChecked={status}>
-                        <input type="checkbox" checked={status} readOnly />
+                    <CheckboxContainer $isChecked={status}>
+                        <input type="checkbox" defaultChecked={status} onChange={() => toggleCheckBox(index)}/>
                     </CheckboxContainer>
                 </TaskItem>
             ))}
@@ -49,33 +58,33 @@ const TaskItem = styled.li`
     border-bottom: 1px solid #ddd;
 `;
 
-const Order = styled.div<{ color: string; isChecked: boolean }>`
+const Order = styled.div<{ color: string, $isChecked: Boolean}>`
     display: block;
     width: 2rem;
     height: 100%;
     text-indent: -9999px;
     padding: 1rem;
-    background-color: ${({ isChecked, color }) => isChecked ? theme.colors.gray : color};
+    background-color: ${({ $isChecked, color }) => $isChecked ? theme.colors.gray : color};
     box-sizing: border-box;
 `;
 
-const Content = styled.span<{ isChecked: boolean }>`
+const Content = styled.span<{$isChecked: Boolean}>`
     flex: 1;
     display: inline-block;
     padding: 1rem;
-    color: ${({ isChecked }) => isChecked ? theme.colors.gray : '#000'};
-    text-decoration: ${({ isChecked }) => isChecked ? 'line-through' : 'unset'};
+    color: ${({ $isChecked }) => $isChecked ? theme.colors.gray : '#000'};
+    text-decoration: ${({ $isChecked }) => $isChecked ? 'line-through' : 'unset'};
 `;
 
-const CheckboxContainer = styled.label<{ isChecked: boolean }>`
+const CheckboxContainer = styled.label<{$isChecked: Boolean}>`
     flex: none;
     ${flexBox({ justify: 'center' })}
     width: 2.4rem;
     height: 2.4rem;
-    background-image: url(${({ isChecked }) => isChecked ? InputCheckComplete.src : InputCheck.src});
+    background-image: url(${({ $isChecked }) => $isChecked ? InputCheckComplete.src : InputCheck.src});
     background-position: center;
     background-size: 100% auto;
     input {
-        opacity: 0;
+        /* opacity: 0; */
     }
 `;
