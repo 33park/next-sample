b28bf853caa994fd5b1ff463b20210f30606cc03
sohyunpark <33.sohyun.park@gmail.com>
2024-03-08 17:41:48 +0900

    컴포넌트 구조 상-하 분기 저장

--- next-basic/src/app/component/todo/TodoCalendar.tsx
+++ next-basic/src/app/component/todo/TodoCalendar.tsx
@@ -1,42 +1,90 @@
-"use client"
-
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { flexBox } from '@/style/styles/common';
-import { theme } from '@/style/styles/theme'
+import { theme } from '@/style/styles/theme';
+
 
 interface TodoProps {
-    calendarList: { date: number; count: number }[];
+    count: number
 }
 
+export default function TodoCalendar({ count }: TodoProps) {
+    // 현재 날짜 상태 추가
+    const [currentDate, setCurrentDate] = useState(new Date());
 
-export default function TodoCalendar({ calendarList }: TodoProps) {
-  return (
-      <>
-      {/* 주말 캘린더 */}
-        <WeekendList>
-            {calendarList.map(({ date, count },index) => (
-                <li key={index}>
-                    <strong>{date}</strong>
-                    <span>{count}</span>
-                </li>
-            ))}
-        </WeekendList>
-    </>
-  )
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            // 현재 날짜를 갱신하여 상태 업데이트
+            const newDate = new Date();
+            setCurrentDate(newDate);
+        }, 60000); // 1분마다 실행
+    
+        // 컴포넌트가 언마운트될 때 interval 정리
+        return () => clearInterval(intervalId);
+    }, []); // 최초 렌더링 시에만 실행
+
+    // 최근 7일의 날짜를 생성하고 포맷팅하는 함수
+    const generateRecentWeekDates = (currentDate: Date) => {
+        const recentWeekDates = [];
+        for (let i = 3; i >= -3; i--) {
+            const date = new Date(currentDate);
+            date.setDate(currentDate.getDate() - i);
+            recentWeekDates.push(date);
+        }
+
+        return recentWeekDates.map(date => {
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '');
+            const day = String(date.getDate()).padStart(2, '');
+            const isToday = date.toDateString() === currentDate.toDateString();
+
+            console.log(`isToday: ${isToday}` );
+            
+
+            return { thisYear: year, thisMonth: month, date: day, count: 0, isToday };
+        });
+    };
+
+    // 최근 7일의 날짜 데이터 생성
+    const formattedRecentWeekDates = generateRecentWeekDates(currentDate);
+
+    return (
+        <>
+            {/* 주말 캘린더 */}
+            <CalendarContainer>
+                <div>
+                    <h2>{currentDate.getFullYear()}</h2>
+                    <h3>{currentDate.getMonth() + 1}</h3>
+                </div>
+                <WeekendList>
+                    {formattedRecentWeekDates.map(({ date, count, isToday },index) => (
+                        <li key={index} currentdatebold={isToday.toString()}>
+                            <strong >{date}</strong>
+                            <span>{count}</span>
+                        </li>
+                    ))}
+                </WeekendList>
+            </CalendarContainer>
+        </>
+    );
 }
 
-const WeekendList = styled.ul`
+const CalendarContainer = styled.div`
+    display: block;
+    width: 100%;
+`;
+
+const WeekendList = styled.ul<{currentdatebold: Boolean}>`
     ${flexBox({justify:'space-around'})}
     width: 100%;
     li{
         ${flexBox()}
         flex-direction: column;
         line-height: 1;
-        background-color: ${theme.colors.light};
+        background-color: ${({ currentdatebold }) => currentdatebold === true ? theme.colors.primary : theme.colors.gray};        
         padding: 0.4rem 0.4rem 0.6rem;
         border-radius: 1rem;
-        color: ${theme.colors.gray};
+        color: ${theme.colors.white};
         font-size: 1.2rem;
 
         strong{
--- next-basic/src/app/todo/page.tsx
+++ next-basic/src/app/todo/page.tsx
@@ -1,53 +1,31 @@
 "use client"
 
-import React,{useState} from 'react'
+import React from 'react';
 import styled from 'styled-components';
 import { flexBox } from '@/style/styles/common';
-import TodoList from '../component/todo/TodoList'
-import TodoCalendar from '../component/todo/TodoCalendar'
+import TodoList from '../component/todo/TodoList';
+import TodoCalendar from '../component/todo/TodoCalendar';
 
 export default function TodoApp() {
 
-        const currentDate = new Date();
 
-        const recentWeekDates = [];
-        for (let i = 0; i < 7; i++) {
-            const date = new Date(currentDate);
-            date.setDate(currentDate.getDate() - i);
-            recentWeekDates.push(date);
-        }
+    // Sample data
+    const registeredList = [
+        { order: 1, content: 'Task 1', status: false },
+        { order: 2, content: 'Task 2', status: true },
+        { order: 3, content: 'Task 3', status: false },
+    ];
 
-        const formattedRecentWeekDates = recentWeekDates.map(date => {
-            // const year = date.getFullYear();
-            // const month = String(date.getMonth() + 1).padStart(2, '0');
-            const day = String(date.getDate()).padStart(2, '');
-
-            console.log(day);
-            
-            return `${day}`;
-        });
-
-        const [calendarList , setCalendar] = useState(formattedRecentWeekDates);
-        
-        // Sample data
-        const registeredList = [
-            { order: 1, content: 'Task 1', status: false },
-            { order: 2, content: 'Task 2', status: true },
-            { order: 3, content: 'Task 3', status: false },
-        ];
-
-        
-  return (
-    <div>
-        <TodoContainer>
-            <h1>Todo List</h1>
-            <TodoCalendar calendarList={formattedRecentWeekDates}/>
-            <TodoList registeredList={registeredList} />
-        </TodoContainer>
-    </div>
-  )
+    return (
+        <div>
+            <TodoContainer>
+                <h1>Todo List</h1>
+                <TodoCalendar calendarList={[]}/>
+                <TodoList registeredList={registeredList} />
+            </TodoContainer>
+        </div>
+    );
 }
-
 
 const TodoContainer = styled.div`
     ${flexBox()}
