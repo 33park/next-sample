ded30908893e358b7efec643dd203cca9543c4e1
sohyunpark <33.sohyun.park@gmail.com>
2024-03-11 17:06:19 +0900

    원복

--- next-basic/src/app/component/todo/TodoCalendar.tsx
+++ next-basic/src/app/component/todo/TodoCalendar.tsx
@@ -2,19 +2,15 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { flexBox } from '@/style/styles/common';
 import { theme } from '@/style/styles/theme';
-import { MoreHorizontal } from 'lucide-react';
-import InputCheck from '@/assets/images/icon/input_check_empty.svg';
-import InputCheckComplete from '@/assets/images/icon/input_checked.svg';
+
 
 interface TodoProps {
-    registeredList: { order: number; content: string; status: boolean }[];
+    count: number
 }
 
-export default function TodoCalendar({ registeredList }: TodoProps) {
-    const [list, setList] = useState(registeredList);
-    const [currentDate, setCurrentDate] = useState(new Date()); // 현재 날짜 상태 추가
-    const [countCheckBox, setCountCheckBox] = useState<number>();
-    
+export default function TodoCalendar({ count }: TodoProps) {
+    // 현재 날짜 상태 추가
+    const [currentDate, setCurrentDate] = useState(new Date());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -52,26 +48,6 @@ export default function TodoCalendar({ registeredList }: TodoProps) {
     // 최근 7일의 날짜 데이터 생성
     const formattedRecentWeekDates = generateRecentWeekDates(currentDate);
 
-    const getOrderColor = (order: number) => {
-        const colors = ['#FFE5A8', '#F8FF97', '#BCFFA4', '#A6FFD3', '#B9E3FF'];
-        return colors[order % 5];
-    };
-
-    const toggleCheckBox = (index: number) => {
-        const updatedList = list.map((item, i) => {
-            if (i === index) {
-                return { ...item, status: !item.status };
-                // spread연산자('...')를 이용하여 새로운 객체복사, 기존 'list 변경하지 않고 새로운 리스트 생성
-            }
-            return item;
-        });
-        setList(updatedList);//상태 업데이트
-        
-        const checkedCount  = updatedList.filter(item => item.status).length; //체크박스 총 갯수
-        setCountCheckBox(checkedCount );
-    }
-
-
     return (
         <>
             {/* 주말 캘린더 */}
@@ -81,26 +57,14 @@ export default function TodoCalendar({ registeredList }: TodoProps) {
                     <h3>{currentDate.getMonth() + 1}</h3>
                 </div>
                 <WeekendList>
-                    {formattedRecentWeekDates.map(({ date,isToday },index) => (
+                    {formattedRecentWeekDates.map(({ date, count, isToday },index) => (
                         <li key={index} currentdatebold={isToday.toString()}>
                             <strong >{date}</strong>
-                            <span>{countCheckBox}</span>
+                            <span>{count}</span>
                         </li>
                     ))}
                 </WeekendList>
             </CalendarContainer>
-            <ListContainer>
-            {list.map(({ order, content, status },index) => (
-                <TaskItem key={order}>
-                    <Order color={getOrderColor(order)} $isChecked={status}>{order}</Order>
-                    <Content $isChecked={status}>{content}</Content>
-                    <MoreHorizontal stroke={theme.colors.gray} />
-                    <CheckboxContainer $isChecked={status}>
-                        <input type="checkbox" defaultChecked={status} onChange={() => toggleCheckBox(index)}/>
-                    </CheckboxContainer>
-                </TaskItem>
-            ))}
-        </ListContainer>
         </>
     );
 }
@@ -126,52 +90,5 @@ const WeekendList = styled.ul<{currentdatebold: Boolean}>`
         strong{
             margin-bottom: .6rem;
         }
-    }
-`;
-
-const ListContainer = styled.ul`
-    ${flexBox()}
-    flex-direction: column;
-    width: 100%;
-    list-style: none;
-    padding: 4rem 2rem;
-`;
-
-const TaskItem = styled.li`
-    flex: 1;
-    ${flexBox()}
-    width: 100%;
-    padding: 1rem 0;
-    border-bottom: 1px solid #ddd;
-`;
-
-const Order = styled.div<{ color: string, $isChecked: Boolean}>`
-    display: block;
-    width: 2rem;
-    height: 100%;
-    text-indent: -9999px;
-    padding: 1rem;
-    background-color: ${({ $isChecked, color }) => $isChecked ? theme.colors.gray : color};
-    box-sizing: border-box;
-`;
-
-const Content = styled.span<{$isChecked: Boolean}>`
-    flex: 1;
-    display: inline-block;
-    padding: 1rem;
-    color: ${({ $isChecked }) => $isChecked ? theme.colors.gray : '#000'};
-    text-decoration: ${({ $isChecked }) => $isChecked ? 'line-through' : 'unset'};
-`;
-
-const CheckboxContainer = styled.label<{$isChecked: Boolean}>`
-    flex: none;
-    ${flexBox({ justify: 'center' })}
-    width: 2.4rem;
-    height: 2.4rem;
-    background-image: url(${({ $isChecked }) => $isChecked ? InputCheckComplete.src : InputCheck.src});
-    background-position: center;
-    background-size: 100% auto;
-    input {
-        opacity: 0;
     }
 `;
--- next-basic/src/app/component/todo/TodoList.tsx
+++ next-basic/src/app/component/todo/TodoList.tsx
@@ -10,55 +10,53 @@ import InputCheck from '@/assets/images/icon/input_check_empty.svg';
 import InputCheckComplete from '@/assets/images/icon/input_checked.svg';
 
 interface TodoProps {
-    todoBoard: {
-        importance: number;
-        content: string;
-        status: Boolean;
-    }[];
+    registeredList: { order: number; content: string; status: boolean }[];
 }
 
-export default function TodoList({ todoBoard }: TodoProps) {
-    const [todoContent, setTodoContent] = useState(todoBoard)
+export default function TodoList({ registeredList }: TodoProps) {
+
+    const [list, setList] = useState(registeredList);
+
+    const getOrderColor = (order: number) => {
+        const colors = ['#FFE5A8', '#F8FF97', '#BCFFA4', '#A6FFD3', '#B9E3FF'];
+        return colors[order % 5];
+    };
+
+    const toggleCheckBox = (index: number) => {
+        const updatedList = list.map((item, i) => {
+            if (i === index) {
+                return { ...item, status: !item.status };
+                // spread연산자('...')를 이용하여 새로운 객체복사, 기존 'list 변경하지 않고 새로운 리스트 생성
+            }
+            return item;
+        });
+        setList(updatedList);
+        //상태 업데이트
+    }
 
     return (
-        <>
-            <div>
-                <CalendarContainer>
-                    <h2>년</h2>
-                    <h3>월</h3>
-                </CalendarContainer>
-                <WeekendList>
-                    <li><span>1</span><span>2</span></li>
-                    <li><span>1</span><span>2</span></li>
-                    <li><span>1</span><span>2</span></li>
-                    <li><span>1</span><span>2</span></li>
-                    <li><span>1</span><span>2</span></li>
-                    <li><span>1</span><span>2</span></li>
-                    <li><span>1</span><span>2</span></li>
-                </WeekendList>
-                <ul>
-                    {todoContent.map(({todoBoard},index)=>(
-                        <TaskItem>
-                            <span>중요도</span>
-                            <p>내용</p>
-                            <label htmlFor=""><input type="checkbox" name="" id="" /></label>
-                        </TaskItem>
-                    ))}
-                </ul>
-            </div>
-        </>
+        <ListContainer>
+            {list && list.map(({ order, content, status },index) => (
+                <TaskItem key={order}>
+                    <Order color={getOrderColor(order)} $isChecked={status}>{order}</Order>
+                    <Content $isChecked={status}>{content}</Content>
+                    <MoreHorizontal stroke={theme.colors.gray} />
+                    <CheckboxContainer $isChecked={status}>
+                        <input type="checkbox" defaultChecked={status} onChange={() => toggleCheckBox(index)}/>
+                    </CheckboxContainer>
+                </TaskItem>
+            ))}
+        </ListContainer>
     );
 }
 
-const CalendarContainer = styled.div`
-    display: block;
+const ListContainer = styled.ul`
+    ${flexBox()}
+    flex-direction: column;
     width: 100%;
+    list-style: none;
+    padding: 4rem 2rem;
 `;
-
-const WeekendList = styled.ul`
-        ${flexBox({justify:'space-around'})}
-    width: 100%;
-`
 
 const TaskItem = styled.li`
     flex: 1;
@@ -66,4 +64,35 @@ const TaskItem = styled.li`
     width: 100%;
     padding: 1rem 0;
     border-bottom: 1px solid #ddd;
+`;
+
+const Order = styled.div<{ color: string, $isChecked: Boolean}>`
+    display: block;
+    width: 2rem;
+    height: 100%;
+    text-indent: -9999px;
+    padding: 1rem;
+    background-color: ${({ $isChecked, color }) => $isChecked ? theme.colors.gray : color};
+    box-sizing: border-box;
+`;
+
+const Content = styled.span<{$isChecked: Boolean}>`
+    flex: 1;
+    display: inline-block;
+    padding: 1rem;
+    color: ${({ $isChecked }) => $isChecked ? theme.colors.gray : '#000'};
+    text-decoration: ${({ $isChecked }) => $isChecked ? 'line-through' : 'unset'};
+`;
+
+const CheckboxContainer = styled.label<{$isChecked: Boolean}>`
+    flex: none;
+    ${flexBox({ justify: 'center' })}
+    width: 2.4rem;
+    height: 2.4rem;
+    background-image: url(${({ $isChecked }) => $isChecked ? InputCheckComplete.src : InputCheck.src});
+    background-position: center;
+    background-size: 100% auto;
+    input {
+        opacity: 0;
+    }
 `;
