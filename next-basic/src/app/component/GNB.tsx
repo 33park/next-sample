0fe2ee03449b7e19319e2e06a416fcfff91fa8fe
sohyunpark <33.sohyun.park@gmail.com>
2024-03-29 13:54:52 +0900

    gpt 제작 todolist 완료

--- next-basic/src/app/component/GNB.tsx
+++ next-basic/src/app/component/GNB.tsx
@@ -16,7 +16,8 @@ interface NavItem {
 const navItems: NavItem[] = [
     { link: '/', text: '메인' },
     { link: '/board', text: '게시판 샘플' },
-    { link: '/todo', text: '투두리스트' }
+    { link: '/todo', text: '투두리스트' },
+    { link: '/test', text: '투두리스트 - 챗gpt' }
 ]
 
 
--- next-basic/src/app/test/reducer.tsx
+++ next-basic/src/app/test/reducer.tsx
@@ -1,4 +1,9 @@
 // reducer.ts
+//
+//Redux를 사용하면 상태를 중앙 집중식으로 관리할 수 있으며, 상태 변경 로직을 별도의 reducer 함수로 분리하여 관리할 수 있습니다. 
+//또한 Redux의 미들웨어를 사용하면 비동기 작업을 효과적으로 처리할 수 있고, 
+//개발자 도구를 통해 상태 변화를 쉽게 추적하고 디버깅할 수 있습니다.
+//
 import { TodoActionTypes, ADD_TODO, COMPLETE_TODO, DELETE_TODO } from './action';
 
 export interface Todo {
